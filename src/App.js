ec2e8256c9f64f632e597ff114cde3d882116570
sumns <sks23info@gmail.com>
2023-04-10 23:37:33 +0530

    Adding greetings msg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const greetings = ["Hello", "Hi", "Hey", "Greetings"];
+  const [greetingIndex, setGreetingIndex] = useState(0);
+
+  const handleButtonClick = () => {
+
+    setGreetingIndex((greetingIndex + 1) % greetings.length);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{greetings[greetingIndex]}</h1>
+      <button onClick={handleButtonClick}>Next Greeting</button>
     </div>
   );
 }
