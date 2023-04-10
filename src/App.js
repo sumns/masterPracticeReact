a7cf81f7c49ebe486eb87fbb1bcfc9964eb609c2
sumns <sks23info@gmail.com>
2023-04-10 21:36:10 +0530

    Adding code to generate random table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+
+  function updateNumbers() {
+    const newNumbers = [...numbers];
+    newNumbers.push(Math.floor(Math.random() * 10));
+    setNumbers(newNumbers);
+  }
+
+  let arr = [1,2,3,4,5,6,7,8,9,10]
+  let random = Math.floor(Math.random() * 10)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={updateNumbers}>Update Numbers</button>
+
+      <p>
+        {
+          arr.map((e)=>(
+            <li>{e*random}</li>
+          ))
+        }
+      </p>
+
     </div>
+
+
   );
 }
 
